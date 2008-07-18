3c6c72803b27f529c5124270bfa2ae611e8b3eca
scytacki <scytacki@4acfd42e-4306-4bc3-9f50-7810ce7e0610>
2008-07-18 04:32:58 +0000

    Fixes to make the home page work again.  These should be applied back to trunk
    git-svn-id: https://svn.concord.org/svn/ccsailportal/branches/itemservice@33 4acfd42e-4306-4bc3-9f50-7810ce7e0610

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,9 @@ class HomeController < ApplicationController
     
     @user = current_user
     # might need to add some check to make sure the user is authenticate
-    @roles_of_user = current_user.space_roles
+    if not @user.nil? 
+      @roles_of_user = current_user.space_roles
+    end    
   end
 
 end
